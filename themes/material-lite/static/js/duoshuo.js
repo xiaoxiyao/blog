8eb7f06713b62739cf6f12fdb4ebc49e20512b32
xiaoxiyao <lxxy2641157419@live.cn>
2015-11-01 02:01:21 +0800

    fix duoshuo

--- themes/material-lite/static/js/duoshuo.js
+++ themes/material-lite/static/js/duoshuo.js
@@ -1014,7 +1014,7 @@ var duoshuoQuery = {
                         var i = s.closest(".ds-ctx-entry, .ds-post-self");
                         h ? h.actionsBar.removeClass("ds-reply-active") : (h = new st.Replybox(e), h.render(!0).el.addClass("ds-inline-replybox").detach()), h.el.find("[name=parent_id]").val(i.attr("data-post-id")), h.el.show().appendTo(s.closest(".ds-ctx-body, .ds-comment-body")).find("textarea").focus(), h.actionsBar = a.addClass("ds-reply-active"), t.max_depth <= 1 ? h.postList = e.postList.el : (h.postList = i.siblings(".ds-children"), h.postList[0] || (h.postList = B('<ul class="ds-children"></ul>').insertAfter(i)))
                     }
-                    upgradeMdl(h.el[0]);
+                    //upgradeMdl(h.el[0]);
                     return !1
                 }
 
@@ -1281,6 +1281,8 @@ var duoshuoQuery = {
                             } catch (e) {}
                         }), m[j] && k.val(m[j])
                     }
+                    console.dir(g[0]);
+                    upgradeMdl(g[0]);
                     return this
                 }
             }, st.Dialog = J.extend({
